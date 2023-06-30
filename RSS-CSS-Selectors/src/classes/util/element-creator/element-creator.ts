df7ed93c545aa838363e2046702f11d38a98dbf7
Minchuk Ilya <cacatubutyt@gmail.com>
2023-06-30 23:23:33 +0200

    feat: add element-creator class

---
+++ RSS-CSS-Selectors/src/classes/util/element-creator/element-creator.ts
@@ -0,0 +1,63 @@
+import {
+  IElementCreatorParam,
+  ICallbackParams,
+  IElementCreator,
+} from "./element-creator-types";
+
+export default class ElementCreator implements IElementCreator {
+  private element: HTMLElement | null;
+
+  constructor(params: IElementCreatorParam) {
+    this.element = null;
+    this.createElement(params);
+  }
+
+  public addInnerElement(innerElement: HTMLElement): void {
+    if (!this.element) {
+      throw new Error("new");
+    }
+    if (innerElement instanceof ElementCreator) {
+      const element: HTMLElement | null = innerElement.getElement();
+      if (element) {
+        this.element.append(element);
+      }
+    } else {
+      this.element.append(innerElement);
+    }
+  }
+
+  public getElement(): HTMLElement | null {
+    return this.element;
+  }
+
+  public createElement(params: IElementCreatorParam): void {
+    this.element = document.createElement(params.tag);
+    this.setCssSelectors(params.cssClasses);
+    this.setTextContent(params.textContent);
+    this.setCallback(params.callback);
+  }
+
+  private setCssSelectors(cssSelectors: string[]): void {
+    if (this.element) {
+      this.element.classList.add(...cssSelectors);
+    }
+  }
+
+  private setTextContent(text: string): void {
+    if (this.element) {
+      this.element.textContent = text;
+    }
+  }
+
+  private setCallback(arg: ICallbackParams | null): void {
+    if (arg) {
+      const { event, callback } = arg;
+      if (!this.element) {
+        throw new Error("new");
+      }
+      if (event) {
+        this.element.addEventListener(event, (e) => callback(e));
+      }
+    }
+  }
+}
