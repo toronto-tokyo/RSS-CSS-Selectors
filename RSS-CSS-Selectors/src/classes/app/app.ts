5fa6104d04d0d31f3ac480f69aa79514942d3aef
Minchuk Ilya <cacatubutyt@gmail.com>
2023-07-01 22:51:26 +0200

    feat: create app component

---
+++ RSS-CSS-Selectors/src/classes/app/app.ts
@@ -0,0 +1,43 @@
+import "./app.css";
+import ElementCreator from "../util/element-creator/element-creator";
+import {
+  IElementCreator,
+  IElementCreatorParam,
+} from "../util/element-creator/element-creator-types";
+import { HeaderView } from "../view/header/header-view";
+import { FooterView } from "../view/footer/footer-veiw";
+import { MainView } from "../view/main/main-view";
+
+const CSS_CLASSES = ["wrapper"];
+export class App {
+  private wrapper: IElementCreator;
+
+  constructor() {
+    const wrapperParams: IElementCreatorParam = {
+      tag: "div",
+      cssClasses: CSS_CLASSES,
+      textContent: "",
+      callback: null,
+    };
+    this.wrapper = new ElementCreator(wrapperParams);
+  }
+
+  public createView(): void {
+    const header = new HeaderView();
+    const headerElement: HTMLElement | null = header.getElement();
+    const footer = new FooterView();
+    const footerElement: HTMLElement | null = footer.getElement();
+    const main = new MainView();
+    const mainElement: HTMLElement | null = main.getElement();
+
+    if (headerElement && footerElement && mainElement) {
+      this.wrapper.addInnerElement(headerElement);
+      this.wrapper.addInnerElement(mainElement);
+      this.wrapper.addInnerElement(footerElement);
+    }
+    const wrapperElement: HTMLElement | null = this.wrapper.getElement();
+    if (wrapperElement) {
+      document.body.append(wrapperElement);
+    }
+  }
+}
