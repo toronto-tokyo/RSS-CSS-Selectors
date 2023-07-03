85c8e7822bd8476f99c79a6231e85312a9534f17
Minchuk Ilya <cacatubutyt@gmail.com>
2023-07-03 17:03:10 +0200

    feat: add bread-slice custom tag

---
+++ RSS-CSS-Selectors/src/data/bread-view/bread-view.ts
@@ -0,0 +1,33 @@
+import "./assets/bread-slice.svg";
+
+const imageLink = "./assets/bread-slice.svg";
+
+export class BreadView extends HTMLElement {
+  constructor(
+    public elementClass: string | undefined,
+    public elementId: string | undefined
+  ) {
+    super();
+    this.configureView(elementClass, elementId);
+  }
+
+  private configureView(
+    elementClass: string | undefined,
+    elementId: string | undefined
+  ): void {
+    this.style.background = `url(${imageLink}) 50%/cover no-repeat`;
+    this.style.width = "100px";
+    this.style.height = "100px";
+    this.style.display = "flex";
+    this.style.alignItems = "center";
+    this.style.justifyContent = "center";
+    if (elementClass) {
+      this.className = elementClass;
+    }
+    if (elementId) {
+      this.id = elementId;
+    }
+  }
+}
+
+customElements.define("bread-slice", BreadView);
