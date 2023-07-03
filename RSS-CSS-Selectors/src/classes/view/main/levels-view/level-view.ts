fe5be8d55959b30217bec3e5a1955466aa57b1aa
Minchuk Ilya <cacatubutyt@gmail.com>
2023-07-03 20:39:15 +0200

    feat: add title and implement selection for levels list

--- RSS-CSS-Selectors/src/classes/view/main/levels-view/level-view.ts
+++ RSS-CSS-Selectors/src/classes/view/main/levels-view/level-view.ts
@@ -10,18 +10,31 @@ import ElementCreator from "../../../util/element-creator/element-creator";
 import { LEVELS_DATA } from "../../../../data/lelels-data";
 
 const CSS_CLASSES = ["levels-view"];
+const CSS_CLASSES_LVL_LINK = ["levels-view__link"];
+const CSS_CLASSES_LVL_TITLE = ["levels-view__title"];
 
 export class LevelsView {
   private element: IElementCreator;
+
+  private linkElements: IElementCreator[];
 
   constructor(tableView: TableView, cssView: CssView, htmlView: HTMLView) {
     const levelsViewParam: IElementCreatorParam = {
       tag: "div",
       textContent: "",
       cssClasses: CSS_CLASSES,
-      callback: null,
+      callback: {
+        event: "click",
+        callback: (event) => {
+          const target = event.target as HTMLDivElement;
+          if (target.className.includes("levels-view__link")) {
+            this.setSelectedLink(target);
+          }
+        },
+      },
     };
     this.element = new ElementCreator(levelsViewParam);
+    this.linkElements = [];
     this.configureView(tableView, cssView, htmlView);
   }
 
@@ -34,10 +47,18 @@ export class LevelsView {
     cssView: CssView,
     htmlView: HTMLView
   ): void {
+    const levelsTitleParam: IElementCreatorParam = {
+      tag: "h2",
+      cssClasses: CSS_CLASSES_LVL_TITLE,
+      textContent: "Choose level",
+      callback: null,
+    };
+    const levelsTitleCreator = new ElementCreator(levelsTitleParam);
+    this.element.addInnerElement(levelsTitleCreator);
     LEVELS_DATA.forEach((levelData) => {
       const levelLinkParam: IElementCreatorParam = {
         tag: "div",
-        cssClasses: [],
+        cssClasses: CSS_CLASSES_LVL_LINK,
         textContent: `${levelData.title}`,
         callback: {
           event: "click",
@@ -46,13 +67,22 @@ export class LevelsView {
             htmlCode.className = "html-code";
             htmlCode.textContent = levelData.htmlCode;
             htmlView.setContent(htmlCode);
-            tableView.setContent(levelData.codeForTable);
+            tableView.setContent(levelData.codeForTable, levelData.help);
             cssView.setContent("");
           },
         },
       };
       const levelLinkCreator = new ElementCreator(levelLinkParam);
+      this.linkElements.push(levelLinkCreator);
       this.element.addInnerElement(levelLinkCreator);
     });
+  }
+
+  private setSelectedLink(link: HTMLElement): void {
+    this.linkElements.forEach((el) => {
+      const element = el.getElement();
+      element?.classList.remove("selected");
+    });
+    link.classList.add("selected");
   }
 }
