495d3f2f63e1802752f18a6660aeb9f270d50f62
Minchuk Ilya <cacatubutyt@gmail.com>
2023-07-05 21:41:12 +0200

    refactor: add LinkCreator

---
+++ RSS-CSS-Selectors/src/classes/util/link-creator/link-creator.ts
@@ -0,0 +1,32 @@
+import { ILinkCreatorParams } from "./link-creator-types";
+
+export class LinkCreator {
+  private linkElement: HTMLAnchorElement;
+
+  constructor(param: ILinkCreatorParams) {
+    this.linkElement = document.createElement("a");
+    this.configureLink(param);
+  }
+
+  public getLinkElement(): HTMLAnchorElement {
+    return this.linkElement;
+  }
+
+  private configureLink(param: ILinkCreatorParams): void {
+    this.setLink(param.href);
+    this.setTargetAttribute(param.target);
+    this.setCssSelectors(param.cssClass);
+  }
+
+  private setLink(link: string): void {
+    this.linkElement.href = link;
+  }
+
+  private setTargetAttribute(targetAttributeValue: string): void {
+    this.linkElement.target = targetAttributeValue;
+  }
+
+  private setCssSelectors(cssSelectors: string[]): void {
+    this.linkElement.classList.add(...cssSelectors);
+  }
+}
